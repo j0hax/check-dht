72431dbc7c8d458d8f468ee1e67adaa3052c18cb
Johannes Arnold <johannes.arnold@stud.uni-hannover.de>
2023-02-16 14:01:59 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# dht22-nagios
+Nagios Plugin for monitoring temperature and humidity from a Raspberry Pi
+
+**Notice:** This plugin works in conjuction with the [dht22](https://github.com/j0hax/dht22) project!
+
+## Install
+This software has two dependencies:
+
+1. [pySerial](https://pyserial.readthedocs.io/en/stable/)
+2. [nagiosplugin](https://nagiosplugin.readthedocs.io/en/stable/)
+
+Both should be available for installation from your distribution's package manager and `pip`.
+
+### openSUSE quick start
+Following commands install dependencies, copy the script to Nagios' plugins and set it as executable:
+
+```console
+$ sudo zypper in python3-pyserial python3-nagiosplugin
+$ cp check_dht.py /usr/lib/nagios/plugins/check_dht
+$ chmod +x /usr/lib/nagios/plugins/check_dht
+```
+
+## Help Overview
+```console
+usage: check_dht [-h] [-p PORT] [-b BAUD] [-w TEMP] [-c TEMP]
+                 [--humidity-warning PERCENT] [--humidity-critical PERCENT]
+
+Nagios Plugin to monitor DHT22/AM2302 data
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -p PORT, --port PORT  the serial file to read from
+  -b BAUD, --baud BAUD  baudrate of the serial port
+  -w TEMP, --warning TEMP
+                        return warning if air temperature is outside TEMP
+  -c TEMP, --critical TEMP
+                        return critical if air temperature is outside TEMP
+  --humidity-warning PERCENT
+                        return warning if humidity is outside PERCENT
+  --humidity-critical PERCENT
+                        return critical if humidity is outside PERCENT
+
+(c) Johannes Arnold 2023. This software is published under the terms of the
+GNU GPLv3. For the companion project to this plugin, see
+https://github.com/j0hax/dht22
+```
+
+
---
+++ check_dht.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+import serial
+import json
+import argparse
+import nagiosplugin
+
+
+def read_data(port, baud):
+    ser = serial.Serial(port, baud)
+    line = ser.readline()
+    return json.loads(line)
+
+
+class DHT(nagiosplugin.Resource):
+    """Domain model: digital temperature and humidity."""
+
+    def __init__(self, port, baud):
+        self.port = port
+        self.baud = baud
+
+    def probe(self):
+        data = read_data(self.port, self.baud)
+        return [
+            nagiosplugin.Metric("temperature", data["temperature"], min=-40, max=80),
+            nagiosplugin.Metric("humidity", data["humidity"], min=0, max=100),
+            nagiosplugin.Metric("error", data["error"], context="default"),
+        ]
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Nagios Plugin to monitor DHT22/AM2302 data",
+        epilog="(c) Johannes Arnold 2023. This software is published under the terms of the GNU GPLv3. For the companion project to this plugin, see https://github.com/j0hax/dht22",
+    )
+
+    parser.add_argument(
+        "-p",
+        "--port",
+        default="/dev/ttyACM0",
+        help="the serial file to read from",
+    )
+
+    parser.add_argument(
+        "-b", "--baud", default=115200, help="baudrate of the serial port"
+    )
+
+    parser.add_argument(
+        "-w",
+        "--warning",
+        metavar="TEMP",
+        default=30,
+        help="return warning if air temperature is outside TEMP",
+    )
+
+    parser.add_argument(
+        "-c",
+        "--critical",
+        metavar="TEMP",
+        default=40,
+        help="return critical if air temperature is outside TEMP",
+    )
+
+    parser.add_argument(
+        "--humidity-warning",
+        metavar="PERCENT",
+        default=40,
+        help="return warning if humidity is outside PERCENT",
+    )
+
+    parser.add_argument(
+        "--humidity-critical",
+        metavar="PERCENT",
+        default=50,
+        help="return critical if humidity is outside PERCENT",
+    )
+
+    args = parser.parse_args()
+
+    check = nagiosplugin.Check(
+        DHT(args.port, args.baud),
+        nagiosplugin.ScalarContext(
+            "temperature", args.warning, args.critical, fmt_metric="{value} °C"
+        ),
+        nagiosplugin.ScalarContext(
+            "humidity",
+            args.humidity_warning,
+            args.humidity_critical,
+            fmt_metric="{value}%",
+        ),
+    )
+
+    check.main()
+
+
+if __name__ == "__main__":
+    main()
